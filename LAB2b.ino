58202513761f19611eb361da5174e0c5826f951d
ut-kr <utkarsh@cyberfear.com>
2023-03-19 11:10:30 +1100

    ✨ dip-switch input
    input dipswitch data and store as int

--- LAB2b.ino
+++ LAB2b.ino
@@ -1,6 +1,7 @@
 // Declaring pins
 int rows[8] = { 14, 15, 16, 17, 18, 19, 20, 21 };
 int cols[8] = { 6, 7, 8, 9, 10, 11, 12, 13 };
+
 void setup() {
   // start serial communication at 9600 baud
   Serial.begin(9600);
@@ -18,5 +19,16 @@ void setup() {
 }
 
 void loop() {
+  static int  num = 0;
 
+  // getting dip-switch data
+  for (int j = 2; j < 6; j++) {
+    int switchState = digitalRead(j);
+    if (not switchState) {
+      num += pow(2, (5-j));
+    }
+  }
+
+  num = 0; // resetting num back to 0
 }
+
