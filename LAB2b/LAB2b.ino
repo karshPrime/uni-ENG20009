65f2b94c2c541aff9cd17bb6ba3244a9a313cdeb
ut-kr <utkarsh@cyberfear.com>
2023-03-20 10:54:17 +1100

    ✨ task completed
    Divided tasks into their own functions: creditTask() and distinctions().
    
    Moved to its own dir; arduino IDE requirement.

--- LAB2b/LAB2b.ino
+++ LAB2b/LAB2b.ino
@@ -21,13 +21,14 @@ void setup() {
 }
 
 void loop() {
-  static bool moveRight = true;
-  static int  start = 0;
-  static int  speedBuffer = 1;
-  static int  speed = 300;
-  static int  num = 0;
+  //creditTask();
+  distinctions(10);
+}
 
-  // getting dip-switch data
+// get num from dip-switch
+void creditTask() {
+  int num = 0;
+
   for (int j = 2; j < 6; j++) {
     int switchState = digitalRead(j);
     if (not switchState) {
@@ -35,116 +36,138 @@ void loop() {
     }
   }
 
+  loadSequence(num, 2);
+}
+
+// move required character
+void distinctions(int num) {
+  static int  start = 2;
+  static int  speedBuffer = 1;
+  static int  speed = 0;
+  static int  speedOption[3] = {50, 100, 200};
+  static bool moveRight = false;
+
+  if (not digitalRead(5)) {
+    moveRight = true;
+    speed = (1.5 * pow(speed, 2)) - (3.5 * speed) + 2;
+    delay(200);
+  }
+  
   // move character if required
   if (moveRight) {
     speedBuffer++;
-    if (speedBuffer > speed) {start += 1; speedBuffer = 0;}
+    if (speedBuffer > speedOption[speed]) {start += 1; speedBuffer = 0;}
     if (start == 9) {start = 0;}
   }
 
-  // different leds for different num
+  loadSequence(num, start);
+}
+
+// decide row and column coords to draw char
+void loadSequence(int num, int start) {
+    // different leds for different num
   if (num == 1) {
     int r[13] = { 0, 0, 1, 1, 2, 3, 4, 5, 6, 7, 7, 7, 7 };
     int c[13] = { 1, 2, 0, 2, 2, 2, 2, 2, 2, 0, 1, 2, 3 };
-    drawChar(13, r, c, start);
+    drawSequence(13, r, c, start);
   }
   else if (num == 2) {
     int r[13] = { 0, 0, 1, 1, 2, 3, 4, 5, 6, 7, 7, 7, 7 };
     int c[13] = { 1, 2, 0, 3, 3, 2, 1, 0, 0, 0, 1, 2, 3 };
-    drawChar(13, r, c, start);
+    drawSequence(13, r, c, start);
   }
   else if (num == 3) {
     int r[13] = { 0, 0, 1, 1, 2, 3, 3, 4, 5, 6, 6, 7, 7 };
     int c[13] = { 1, 2, 0, 3, 3, 1, 2, 3, 3, 0, 3, 1, 2 };
-    drawChar(13, r, c, start);
+    drawSequence(13, r, c, start);
   }
   else if (num == 4) {
     int r[13] = { 0, 1, 2, 2, 3, 3, 4, 4, 4, 4, 5, 6, 7 };
     int c[13] = { 0, 0, 0, 3, 0, 3, 0, 1, 2, 3, 3, 3, 3 };
-    drawChar(13, r, c, start);
+    drawSequence(13, r, c, start);
   }
   else if (num == 5) {
     int r[15] = { 0, 0, 0, 0, 1, 2, 3, 3, 3, 4, 5, 6, 6, 7, 7 };
     int c[15] = { 0, 1, 2, 3, 0, 0, 0, 1, 2, 3, 3, 0, 3, 1, 2 };
-    drawChar(15, r, c, start);
+    drawSequence(15, r, c, start);
   }
   else if (num == 6) {
     int r[15] = { 0, 0, 1, 1, 2, 3, 4, 4, 4, 5, 5, 6, 6, 7, 7 };
     int c[15] = { 1, 2, 0, 3, 0, 0, 0, 1, 2, 0, 3, 0, 3, 1, 2 };
-    drawChar(15, r, c, start);
+    drawSequence(15, r, c, start);
   }
   else if (num == 7) {
     int r[11] = { 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7 };
     int c[11] = { 0, 1, 2, 3, 3, 3, 2, 1, 1, 1, 1 };
-    drawChar(11, r, c, start);
+    drawSequence(11, r, c, start);
   }
   else if (num == 8) {
     int r[16] = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
     int c[16] = { 1, 2, 0, 3, 0, 3, 1, 2, 0, 3, 0, 3, 0, 3, 1, 2 };
-    drawChar(16, r, c, start);
+    drawSequence(16, r, c, start);
   }
   else if (num == 9) {
     int r[15] = { 0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 5, 6, 6, 7, 7 };
     int c[15] = { 1, 2, 0, 3, 0, 3, 1, 2, 3, 3, 3, 0, 3, 1, 2 };
-    drawChar(15, r, c, start);
+    drawSequence(15, r, c, start);
   }
   else if (num == 10) {
     int r[20] = { 0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
     int c[20] = { 1, 2, 3, 0, 4, 0, 4, 0, 1, 2, 3, 4, 0, 4, 0, 4, 0, 4, 0, 4 };
-    drawChar(20, r, c, start);
+    drawSequence(20, r, c, start);
   }
   else if (num == 11) {
     int r[22] = { 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 7 };
     int c[22] = { 0, 1, 2, 3, 0, 4, 0, 4, 0, 1, 2, 3, 0, 3, 0, 4, 0, 4, 0, 1, 2, 3 };
-    drawChar(22, r, c, start);
+    drawSequence(22, r, c, start);
   }
   else if (num == 12) {
     int r[12] = { 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 7, 7 };
     int c[12] = { 2, 3, 4, 1, 0, 0, 0, 0, 1, 2, 3, 4 };
-    drawChar(12, r, c, start);
+    drawSequence(12, r, c, start);
   }
   else if (num == 13) {
     int r[18] = { 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7 };
     int c[18] = { 0, 1, 2, 0, 3, 0, 4, 0, 4, 0, 4, 0, 4, 0, 3, 0, 1, 2 };
-    drawChar(18, r, c, start);
+    drawSequence(18, r, c, start);
   }
   else if (num == 14) {
     int r[17] = { 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 7, 7, 7 };
     int c[17] = { 1, 2, 3, 4, 0, 0, 0, 0, 1, 2, 3, 0, 0, 1, 2, 3, 4 };
-    drawChar(17, r, c, start);
+    drawSequence(17, r, c, start);
   }
   else if (num == 15) {
     int r[14] = { 0, 0, 0, 0, 1, 2, 3, 4, 4, 4, 4, 5, 6, 7 };
     int c[14] = { 1, 2, 3, 4, 0, 0, 0, 0, 1, 2, 3, 0, 0, 0 };
-    drawChar(14, r, c, start);
+    drawSequence(14, r, c, start);
   }
-  else {
+  else { // zero
     int r[16] = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
     int c[16] = { 1, 2, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 1, 2 };
-    drawChar(16, r, c, start);
+    drawSequence(16, r, c, start);
   }
-
-  num = 0; // resetting num back to 0
 }
 
-void drawChar(int arrayLen, int* r, int* c, int start) {
+// take in rows and columns sequence and get each of them drawn
+void drawSequence(int arrayLen, int* r, int* c, int start) {
   int cPos;
   for (int i = 0; i < arrayLen; i++) {
     if (start+c[i] > 9) { cPos = start+c[i] - 9; } else { cPos = start+c[i]; }
-    onLED(cPos, r[i]); 
+    singleLED(cPos, r[i]); 
   }
 }
 
-void onLED(int x, int y) {
+// turn on single led
+void singleLED(int x, int y) {
   ledreset();
   digitalWrite(rows[y], HIGH);
   digitalWrite(cols[x], LOW);
 }
 
+// reset grid
 void ledreset() {
   for (int i = 0; i < 8; i++) {
     digitalWrite(rows[i], LOW);
     digitalWrite(cols[i], HIGH);
   }
 }
-
